2dae53a23de9f6243cd7b999d4e5446cfa4f36e3
Leonard Colin <leooo.colin@gmail.com>
2023-12-02 14:38:00 +0100

    refacto

--- python/2023/day1/day1.py
+++ python/2023/day1/day1.py
@@ -1,25 +1,30 @@
 import os
+import re
 
-puzzle_file = os.path.join(os.environ.get("BASE_DIR"), "2023/day1/puzzle.txt")
+PUZZLE_FILE = os.path.join(os.environ.get("BASE_DIR"), "2023/day1/puzzle.txt")
 
-with open(puzzle_file, 'r') as file:
-    data = [line.strip() for line in file]
+def read_puzzle():
+    with open(PUZZLE_FILE, 'r') as file:
+        return [line.strip() for line in file]
 
-cal_values = []
-for line in data:
-    for i in line:
-        try:
-            first_num = int(i)
-            break
-        except ValueError:
-            continue
-    for i in reversed(line):
-        try:
-            last_num = int(i)
-            break
-        except ValueError:
-            continue
+# part 1
+def day_one_part_one():
+    data = read_puzzle()
+    cal_values = []
+    for line in data:
+        for i in line:
+            try:
+                first_num = int(i)
+                break
+            except ValueError:
+                continue
+        for i in reversed(line):
+            try:
+                last_num = int(i)
+                break
+            except ValueError:
+                continue
 
-    cal_values.append(int(f"{first_num}{last_num}"))
+        cal_values.append(int(f"{first_num}{last_num}"))
+    print(cal_values)
 
-print(sum(cal_values))

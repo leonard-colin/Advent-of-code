26af13e04d63b607fec636e599866c00de857410
Leonard Colin <leooo.colin@gmail.com>
2023-12-03 00:51:40 +0100

    2023 day1 part2

--- python/2023/day1/day1.py
+++ python/2023/day1/day1.py
@@ -26,5 +26,35 @@ def day_one_part_one():
                 continue
 
         cal_values.append(int(f"{first_num}{last_num}"))
-    print(cal_values)
+    print(sum(cal_values))
+
+# part 2
+def day_one_part_two():
+    data = read_puzzle()
+    cal_values = []
+    letter_to_digit =  {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
+    pattern = r"(?=(one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9))"
+    for line in data:
+        first_num = re.findall(pattern, line)[0]
+        last_num = re.findall(pattern, line)[-1]
+
+        first_num = first_num if first_num.isdigit() else letter_to_digit[first_num]
+        last_num = last_num if last_num.isdigit() else letter_to_digit[last_num]
+
+        cal_values.append(int(f"{first_num}{last_num}"))
+
+    print(sum(cal_values))
+
+day_one_part_one()
+day_one_part_two()
 
